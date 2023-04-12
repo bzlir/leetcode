34896630804ca7c59e2d57e2e31c9b1875304505
bzlir <118650436+bzlir@users.noreply.github.com>
2023-04-13 06:47:14 +0800

    linked_list (#31)

--- utils.py
+++ utils.py
@@ -37,7 +37,10 @@ class PlanGenerator:
 
         return set(sorted(result))
 
-    
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 if __name__ == '__main__':
     config_path = 'config.yaml'
