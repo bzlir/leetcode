6ff3bf80b985060ff3c96f153e147cf2a7347205
bzlir <bnline@outlook.com>
2023-04-18 07:21:06 +0800

    dp

---
+++ dp/91.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = {len(s) : 1}
+
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            if i < len(s)-1 and (s[i] == '1' or (s[i] == '2' and s[i+1] < '7')):
+                dp[i] += dp[i+2]
+        return dp[0]
